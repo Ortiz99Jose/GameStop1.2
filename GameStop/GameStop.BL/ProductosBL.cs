3e8a7bd69e6f169b01618943f867106206b8c6b2
josec <josec@GENIUS-PC>
2022-07-05 00:42:33 -0600

    Agregando arquitectura basica

---
+++ GameStop/GameStop.BL/ProductosBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStop.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Fifa 2016";
+            producto1.Precio = 1200;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Fifa 2022";
+            producto2.Precio = 1500;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+
+            return listadeProductos;
+        }
+    }
+}
--- GameStop/GameStop.web/Controllers/ProductosController.cs
+++ GameStop/GameStop.web/Controllers/ProductosController.cs
@@ -1,9 +1,10 @@
-﻿using GameStop.web.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GameStop.BL;
 
 namespace GameStop.web.Controllers
 {
@@ -12,17 +13,8 @@ namespace GameStop.web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1 ;
-            producto1.Descripcion = "Fifa 2016";
-          
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Fifa 2016";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
