d15db4d40c89e7fcaebbffd81c9ab015b2e2b0a4
josec <josec@GENIUS-PC>
2022-07-06 00:27:48 -0600

    Agregamos Entity Framework Code First

--- GameStop/GameStop.BL/ProductosBL.cs
+++ GameStop/GameStop.BL/ProductosBL.cs
@@ -8,23 +8,19 @@ namespace GameStop.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Fifa 2016";
-            producto1.Precio = 1200;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Fifa 2022";
-            producto2.Precio = 1500;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
         }
     }
 }
